5e951b8b54479412393f36a375c05e7e6ad5a1c1
JCDHorse <cdlith@proton.me>
2025-12-09 15:57:17 +0100

    change: private properties naming

--- main.py
+++ main.py
@@ -3,4 +3,13 @@ from classVelo import Velo
 from Station import Station
 
 parc = ParcDeVelos()
+s_eiffel = Station("Tour Eiffel", "5 parc du Champ de Mars, 75007 Paris")
+s_eiffel.ajouter_velo(10)
+s_louvre = Station("Louvre", "8 rue Sainte-Anne, 75001 Paris")
 
+parc.ajouter_station(s_eiffel)
+parc.ajouter_station(s_louvre)
+
+ok = parc.louer_velo(s_eiffel.id, 7)
+if ok:
+    print("Vélo loué avec succès")
--- parcvelos.py
+++ parcvelos.py
@@ -3,13 +3,13 @@ from classVelo import Velo
 
 class ParcDeVelos:
     def __init__(self):
-        self._stations = dict()
-        self._en_locations = []
-        self._en_reparations = []
+        self.__stations = dict()
+        self.__en_locations = []
+        self.__en_reparations = []
 
     def trouve_velo(self, v_id: str) -> Velo | None:
         velo: Velo | None = None
-        for station in self._stations:
+        for station in self.__stations:
             for s_velo in station.velos:
                 if s_velo.get_id() == v_id:
                     velo = s_velo
@@ -29,14 +29,17 @@ class ParcDeVelos:
 
     def creer_station(self, name: str, address: str, capacity: int = 10):
         station = Station(name, address, capacity)
-        self._stations[station.id] = station
+        self.__stations[station.id] = station
 
     def ajouter_station(self, station: Station):
-        self._stations[station.id] = station
+        self.__stations[station.id] = station
 
     def louer_velo(self, s_id: str, loc_debut: int):
-        velo = self._stations[s_id].retirer_velo()
-        self._en_locations.append(velo)
+        velo = self.__stations[s_id].retirer_velo()
+        if velo is None:
+            return False
+        self.__en_locations.append(velo)
+        return True
 
     def retourner_velo(self, v_id: str, loc_fin: int) -> bool:
         velo = self.trouve_velo(v_id)
@@ -45,11 +48,11 @@ class ParcDeVelos:
             return False
 
         velo.retourner()
-        self._en_locations.remove(velo)
+        self.__en_locations.remove(velo)
         return True
 
     def ajouter_new_velos(self, s_id: str, nv: int):
-        self._stations[s_id].ajouter_velo(nv)
+        self.__stations[s_id].ajouter_velo(nv)
 
     def envoyer_en_reparation(self, v_id: str) -> bool:
         velo = self.trouve_velo(v_id)
@@ -58,7 +61,7 @@ class ParcDeVelos:
             return False
 
         velo.a_reparer()
-        self._en_reparations.append(velo)
+        self.__en_reparations.append(velo)
         return True
 
     def reaffecter_velo_repare(self, v_id: str) -> bool:
@@ -68,11 +71,11 @@ class ParcDeVelos:
             return False
 
         velo.terminer_reparation()
-        self._en_reparations.remove(velo)
+        self.__en_reparations.remove(velo)
         return True
 
     def consulter_parc(self):
         infos = ""
-        for station in self._stations:
+        for station in self.__stations:
             infos += station.afficher_info()
         return infos
