9a1907e75cbbc5f8687899376330d6d2e1d161e9
Yoan.L <y.lespessailles@outlook.fr>
2025-12-09 14:19:09 +0100

    Ajout du fichier classVelo.py

---
+++ classVelo.py
@@ -0,0 +1,52 @@
+import random
+
+class Velo:
+  __id_velo=[]
+
+  def __init__(self):
+    self.__id=self.random_id()
+    self.__etat="disponible"
+    self.__indice=0
+
+  def get_id(self):
+    return self.__id
+  
+  def get_etat(self):
+    return self.__etat
+  
+  def get_indice(self):
+    return self.__indice
+
+  def random_id(self):
+    while True:
+      random_nb = random.randint(2400, 3000)
+      if random_nb not in Velo.__id_velo:
+        Velo.__id_velo.append(random_nb)
+        return f"V_{random_nb}"
+  
+  def mettre_en_location(self):
+    if self.__etat == "disponible":
+      self.__etat = "en_location"
+      self.__indice+=1
+    else:
+      return f"Etat du vélo : {self.__etat}"
+
+  def retourner(self):
+    if self.__etat == "en_location":
+      self.__etat = "disponible"
+    else:
+      return f"Etat du vélo : {self.__etat}"
+
+  def a_reparer(self):
+    if self.__etat != "en_reparation":
+      self.__etat = "en_reparation"
+      self.__indice+=10
+  
+  def terminer_reparation(self):
+    if self.__etat == "en_reparation":
+      self.__etat = "disponible"
+    else:
+      return f"Etat du vélo : {self.__etat}"
+
+  def __str__(self):
+    return f"Velo(id={self.__id}, etat={self.__etat}, indice={self.__indice})"
