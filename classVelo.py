23b2d819804d7e6ef36f2cec52ebe4de86b69f87
JCDHorse <cdlith@proton.me>
2025-12-14 14:55:54 +0100

    refactor: utilisation d'enums

--- classVelo.py
+++ classVelo.py
@@ -1,61 +1,65 @@
 import random
+from enum import Enum
+
+class VeloEtat(Enum):
+    DISPONIBLE = 0
+    LOUEE = 1
+    REPARATION = 2
 
 class Velo:
   __id_velo=[]
 
+  @staticmethod
+  def random_id():
+      while True:
+          random_nb = random.randint(2400, 3000)
+          if random_nb not in Velo.__id_velo:
+              Velo.__id_velo.append(random_nb)
+              return f"V_{random_nb}"
+
   def __init__(self):
-    self.__id=self.random_id()
-    self.__etat="disponible"
-    self.__indice=0
+    self.__id: str = self.random_id()
+    self.__etat: VeloEtat = VeloEtat.DISPONIBLE
+    self.__indice: int = 0
 
-  def get_id(self):
+  @property
+  def id(self) -> str:
     return self.__id
-  
-  id=property(fget=get_id)
 
-  def get_etat(self):
+  @property
+  def etat(self) -> VeloEtat:
     return self.__etat
-  
-  etat=property(fget=get_etat)
-  
-  def get_indice(self):
+
+  @property
+  def indice(self) -> int:
     return self.__indice
 
-  indice=property(fget=get_indice)
-
-  def random_id(self):
-    while True:
-      random_nb = random.randint(2400, 3000)
-      if random_nb not in Velo.__id_velo:
-        Velo.__id_velo.append(random_nb)
-        return f"V_{random_nb}"
-  
   def mettre_en_location(self):
-    if self.__etat == "disponible":
-      self.__etat = "en_location"
+    if self.__etat == VeloEtat.DISPONIBLE:
+      self.__etat = VeloEtat.LOUEE
       self.__indice+=1
       return True
     else:
       return False
 
   def retourner(self):
-    if self.__etat == "en_location":
-      self.__etat = "disponible"
+    if self.__etat == VeloEtat.LOUEE:
+      self.__etat = VeloEtat.DISPONIBLE
       return True
     else:
       return False
 
   def a_reparer(self):
-    if self.__etat != "en_reparation":
-      self.__etat = "en_reparation"
+    if self.__etat != VeloEtat.REPARATION:
+      self.__etat = VeloEtat.REPARATION
       self.__indice+=10
       return True
     else:
       return False
   
   def terminer_reparation(self):
-    if self.__etat == "en_reparation":
-      self.__etat = "disponible"
+    if self.__etat == VeloEtat.REPARATION:
+      self.__etat = VeloEtat.DISPONIBLE
       return True
     else:
       return False
