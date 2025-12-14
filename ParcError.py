834556817c9b8e15e4af74c6b2896303787481bc
JCDHorse <cdlith@proton.me>
2025-12-14 16:06:50 +0100

    added comments

--- ParcError.py
+++ ParcError.py
@@ -1,4 +1,7 @@
 
+# Création d'exception pour une gestion d'erreurs plus simple
+# Bonus non demandé par le TP
+
 class ParcError(Exception):
     def __init__(self, msg: str):
         super().__init__(f"{msg}")
